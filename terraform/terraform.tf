5550cf1e4f3554df8666c7cd94032af0f4c0be8d
prakashghropade <prakashghorpade2001@gmail.com>
2026-07-03 10:08:09 +0530

    updated the backend terraform file

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-s3-backend-tws-hackathon"
-    key    = "backend-locking"
-    region = "ap-south-1"
-    use_lockfile = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-s3-backend-tws-hackathon"
+#     key    = "backend-locking"
+#     region = "ap-south-1"
+#     use_lockfile = true
+#   }
+# }
